a1b78828886611b1e15da7dac0042e388b27c7a3
PaulTerokhin <terohinp@gmail.com>
2023-01-28 16:11:50 +0200

    first attempt :)

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int number1 = 8, number2 = 3;
+        char sign = '^';
+        int result = number1 + number2;
+
+        if (sign == '-') {
+            result = number1 - number2;
+        } else if (sign == '*') {
+            result = number1 * number2;
+        } else if (sign == '/') {
+            result = number1 / number2;
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 1; i <= number2; i++) {
+                result *= number1;
+            }
+        } else if (sign == '%') {
+            result = number1 % number2;
+        }
+
+        System.out.println(number1 + " " + sign + " " + number2 + " = " + result);
+    }
+}
+
