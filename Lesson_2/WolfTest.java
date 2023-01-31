24fe0818f1e516e49c1effdc8e8004d42cc40f0b
PaulTerokhin <terohinp@gmail.com>
2023-01-31 19:05:14 +0200

    WolfTest

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,28 +1,5 @@
-class Wolf {
-    String name;
-    int weightInKg;
-    int age;
-    String color;
-    String gender;
-
-    void move() {
-        System.out.println("moving");
-    }
-    void sit() {
-        System.out.println("sitting");
-    }
-    void run() {
-        System.out.println("running");
-    }
-    void howl() {
-        System.out.println("howling");
-    }
-    void hunt() {
-        System.out.println("hunting");
-    }
-}
-
 public class WolfTest {
+    
     public static void main(String[] args) {
         Wolf luk = new Wolf();
         luk.name = "Luk";
