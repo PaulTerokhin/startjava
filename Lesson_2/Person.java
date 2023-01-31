493527bcf7ffdb32756ca1f6931a3cc9efe6fe7b
PaulTerokhin <terohinp@gmail.com>
2023-01-31 13:17:25 +0200

    Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,24 @@
+
+public class Person {
+    String name = "Michael";
+    int age = 25;
+    int weightInKg = 80;
+    int heightInSm = 180;
+    String gender = "Male";
+
+    void move() {
+        System.out.println("moving");
+    }
+    void sit() {
+        System.out.println("sitting");
+    }
+    void run() {
+        System.out.println("running");
+    }
+    void speak() {
+        System.out.println("speaking");
+    }
+    void learnJava() {
+        System.out.println("learning Java");
+    }
+}
