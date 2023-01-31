0938c2f7d58f839df53b1b0ea63755484cfe3ab5
PaulTerokhin <terohinp@gmail.com>
2023-01-31 19:06:19 +0200

    Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,23 @@
+public class Wolf {
+    String name;
+    int weightInKg;
+    int age;
+    String color;
+    String gender;
+
+    void move() {
+        System.out.println("moving");
+    }
+    void sit() {
+        System.out.println("sitting");
+    }
+    void run() {
+        System.out.println("running");
+    }
+    void howl() {
+        System.out.println("howling");
+    }
+    void hunt() {
+        System.out.println("hunting");
+    }
+}
