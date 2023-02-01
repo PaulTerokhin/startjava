4c984f12d950d2dde9c79ae94e2bb3978f615164
PaulTerokhin <terohinp@gmail.com>
2023-02-01 16:10:42 +0200

    Getters

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,14 +1,16 @@
 public class WolfTest {
-    
+
     public static void main(String[] args) {
         Wolf luk = new Wolf();
-        luk.name = "Luk";
-        luk.weightInKg = 40;
-        luk.age = 5;
-        luk.color = "серый";
-        luk.gender = "мужской";
-        System.out.println("Кличка " + luk.name + "\nПол " + luk.gender + "\nВес " +
-                luk.weightInKg + "\nВозраст " + luk.age + "\nОкрас " + luk.color); 
+        luk.setName("Luk");
+        luk.setWeightInKg(40);
+        luk.setAge(5);
+        luk.setColor("серый");
+        luk.setGender("мужской");
+
+        System.out.println("Кличка " + luk.getName() + "\nПол " + luk.getGender() + "\nВес " +
+                luk.getWeightInKg() + "\nВозраст " + luk.getAge() + "\nОкрас " + luk.getColor()); 
+
         luk.move();
         luk.sit();
         luk.run();
