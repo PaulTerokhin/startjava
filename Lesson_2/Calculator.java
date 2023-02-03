34794a1691716a6b29739a6a9e5a54475919bb9a
PaulTerokhin <terohinp@gmail.com>
2023-02-03 16:17:14 +0200

    changes

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,31 +1,24 @@
 public class Calculator {
 
     public static int calculation (int number1, int number2, char operation) {
-        int result = 0;
-
+        int result = 1;
         switch(operation) {
             case '-':
-                result = number1 - number2;
-                break;
+                return number1 - number2;
             case '+':
-                result = number1 + number2;
-                break;
+                return number1 + number2;
             case '*':
-                result = number1 * number2;
-                break;
+                return number1 * number2;
             case '/':
-                result = number1 / number2;
-                break;
+                return number1 / number2;
             case '^':
-                result = 1;
                 for (int i = 1; i <= number2; i++) {
                     result *= number1;
                 }
-                break;
+                return result;
             case '%':
-                result = number1 % number2;
-                break;
+                return number1 % number2;
         }
-        return result;
+    return result;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,50 +4,44 @@ class CalculatorTest {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String choice;
+        String retry;
         do {
-            int number1 = getInt1();
-            char operation = getOperation();
-            int number2 = getInt2();
-            int result = Calculator.calculation(number1, number2, operation);
+            int number1 = inputNumber();
+            char operation = inputOperation();
+            int number2 = inputNumber();
+            Calculator test = new Calculator();
+            int result = test.calculation(number1, number2, operation);
             System.out.println("Результат операции: " + result);
-
-            System.out.println("Хотите продолжить вычисления? [yes/no]:");
-            choice = scan.next();
-            if (choice.equals("yes")) {
-                System.out.println("Давай еще раз");
-            } else if (choice.equals("no")) {
-                System.exit(0);
-            } else {
-                System.out.print("Введите yes или no\n");
-                choice = scan.next();
-            }
-        } while (choice.equals("yes"));
+            retry = inputReply();
+        } while (retry.equals("yes"));
     }
 
-    public static int getInt1() {
-        System.out.println("Введите первое число");
-        int number1 = scan.nextInt();
-        while (number1 <= 0 ) {
+    private static int inputNumber() {
+        System.out.println("Введите число");
+        int number = scan.nextInt();
+        while (number <= 0 ) {
             System.out.print("Ваше число должно быть положительным.Введите число заново\n");
-            number1 = scan.nextInt();
+            number = scan.nextInt();
         }
-        return number1;
+        return number;
     }
 
-    public static char getOperation(){
+    private static char inputOperation() {
         System.out.println("Введите операцию:");
         char operation = scan.next().charAt(0);
         return operation;
     }
 
-    public static int getInt2() {
-        System.out.println("Введите второе число");
-        int number2 = scan.nextInt();
-        while (number2 <= 0 ) {
-            System.out.print("Ваше число должно быть положительным.Введите число заново\n");
-            number2 = scan.nextInt();
+    private static String inputReply() {
+        System.out.println("Хотите продолжить вычисления? [yes/no]:");
+        String choice = scan.next();
+        if(choice.equals("no")) {
+            System.out.println("Спасибо, что воспользовались калькулятором");
+        } else if(choice.equals("yes")) {
+            System.out.println("Давайте попробуем еще раз :)");
+        } else {
+            inputReply();
         }
-        return number2;
+        return choice;
     }
 }
