7f9b23bdb3fbb33345675c44e3ab68b25dca3d04
PaulTerokhin <terohinp@gmail.com>
2023-02-04 22:02:58 +0200

    Jaeger

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,28 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger gipsy = new Jaeger();
+        gipsy.setModelName("Gipsy Danger");
+        gipsy.setOrigin("USA");
+        gipsy.setHeight(79.25f);
+        gipsy.setWeight(1.980f);
+        gipsy.setSpeed(7);
+        gipsy.setArmor(6);
+
+        System.out.println("Model Name: " + gipsy.getModelName() + "\nOrigin: " + 
+                gipsy.getOrigin() + "\nHeight: " + gipsy.getHeight() + "\nSpeed: " + 
+                gipsy.getSpeed() + "\nArmor: " + gipsy.getArmor());
+
+        gipsy.drift();
+        gipsy.move();
+        gipsy.useVortexCannon();
+        gipsy.startShooting();
+
+        Jaeger coyote = new Jaeger("Coyote Tango", "USA", 85.34f, 2.312f, 5, 4);
+
+        coyote.drift();
+        coyote.move();
+        coyote.useVortexCannon();
+        coyote.startShooting();
+    }
+}
