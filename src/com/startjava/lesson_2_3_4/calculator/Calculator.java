f51a8656fbf85b3cfab7f1d36b925f52a249e517
PaulTerokhin <terohinp@gmail.com>
2023-02-19 15:50:22 +0200

    Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,24 +2,20 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public static int calculation(int number1, int number2, char operation) {
+    public static double calculate(int number1, int number2, char operation) {
         switch(operation) {
             case '-':
-                return number1 - number2;
+                return Math.subtractExact(number1, number2);
             case '+':
-                return number1 + number2;
+                return Math.addExact(number1, number2);
             case '*':
-                return number1 * number2;
+                return Math.multiplyExact(number1, number2);
             case '/':
-                return number1 / number2;
+                return (double) number1 / number2;
             case '^':
-                int result = 1;
-                for (int i = 1; i <= number2; i++) {
-                    result *= number1;
-                }
-                return result;
+                return Math.pow((double) number1, (double) number2);
             case '%':
-                return number1 % number2;
+                return Math.floorMod(number1, number2);
         }
         return 0;
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -8,38 +8,37 @@ class CalculatorTest {
     public static void main(String[] args) {
         String userAnswer;
         do {
-            int number1 = inputNumber();
-            char operation = inputOperation();
-            int number2 = inputNumber();
+            String[] expression = inputMathExpression();
             Calculator calc = new Calculator();
-            int result = calc.calculation(number1, number2, operation);
-            System.out.println("Результат операции: " + result);
+            double result = calc.calculate(Integer.parseInt(expression[0]),
+                    Integer.parseInt(expression[2]), expression[1].charAt(0));
+            showResult(result);
             userAnswer = inputReply();
-        } while (userAnswer.equals("yes"));
+        } while(userAnswer.equals("yes"));
     }
 
-    private static int inputNumber() {
-        System.out.println("Введите число");
-        int number = scan.nextInt();
-        while (number <= 0) {
-            System.out.print("Ваше число должно быть положительным.Введите число заново\n");
-            number = scan.nextInt();
+    private static String[] inputMathExpression() {
+        System.out.println("\nВведите выражение формата: 2 ^ 10");
+        String string = scan.nextLine();
+        return string.split(" ");
+    }
+
+    private static void showResult(double result) {
+        System.out.print("Результат вычислений: ");
+        if (result % 1 == 0) {
+            System.out.println((int) result);
+        } else {
+            System.out.printf("%.3f\n", result);
         }
-        return number;
-    }
-
-    private static char inputOperation() {
-        System.out.println("Введите операцию:");
-        return scan.next().charAt(0);
     }
 
     private static String inputReply() {
         System.out.println("Хотите продолжить вычисления? [yes/no]:");
-        String choice = scan.next();
-        if(choice.equals("no")) {
+        String choice = scan.nextLine();
+        if (choice.equals("no")) {
             System.out.println("Спасибо, что воспользовались калькулятором");
-        } else if(choice.equals("yes")) {
-            System.out.println("Давайте попробуем еще раз :)");
+        } else if (choice.equals("yes")) {
+            System.out.print("Давайте попробуем еще раз :)");
         } else {
             inputReply();
         }
