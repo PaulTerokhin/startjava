f8a74533fda740d6054513c921a49761b1b2b403
PaulTerokhin <terohinp@gmail.com>
2023-02-22 16:57:14 +0200

    Calculator

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,44 +1,21 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.util.Scanner;
-
 public class Calculator {
 
     public static double calculate(String mathExpression) {
-        String[] expression = mathExpression.split(" ");
-        int number1 = checkNumber(expression[0]);
-        int number2 = checkNumber(expression[2]);
-        char operation = expression[1].charAt(0);
+        String[] partsExpression = mathExpression.split(" ");
+        int number1 = CalculatorTest.checkNumber(partsExpression[0]);
+        int number2 = CalculatorTest.checkNumber(partsExpression[2]);
+        char operation = partsExpression[1].charAt(0);
 
-        switch(operation) {
-            case '-' -> {return Math.subtractExact(number1, number2);}
-            case '+' -> {return Math.addExact(number1, number2);}
-            case '*' -> {return Math.multiplyExact(number1, number2);}
-            case '/' -> {return (double) number1 / number2;}
-            case '^' -> {return Math.pow(number1, number2);}
-            case '%' -> {return Math.floorMod(number1, number2);}
-        }
-        return 0;
-    }
-
-    private static int checkNumber(String positiveInteger) {
-        int number;
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            try {
-                number = Integer.parseInt(positiveInteger);
-                if (number <= 0) {
-                    throw new Exception("Число должнно быть положительным!");
-                }
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Ошибка: число " + positiveInteger + " должно быть целым. Введите число заново");
-                positiveInteger = scan.next();
-            } catch (Exception e) {
-                System.out.println("Ошибка: число " + positiveInteger + " должно быть положительным. Введите число заново");
-                positiveInteger = scan.next();
-            }
-        }
-        return number;
+        return switch(operation) {
+            case '-' -> Math.subtractExact(number1, number2);
+            case '+' -> Math.addExact(number1, number2);
+            case '*' -> Math.multiplyExact(number1, number2);
+            case '/' -> (double) number1 / number2;
+            case '^' -> Math.pow(number1, number2);
+            case '%' -> Math.floorMod(number1, number2);
+            default -> throw new IllegalArgumentException("Введен некорректный знак");
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -20,6 +20,27 @@ class CalculatorTest {
         return scan.nextLine();
     }
 
+    static int checkNumber(String positiveInteger) {
+        int number;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            try {
+                number = Integer.parseInt(positiveInteger);
+                if (number <= 0) {
+                    throw new IllegalArgumentException("Число должно быть положительным!");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: число " + positiveInteger + " должно быть целым. Введите число заново");
+                positiveInteger = scan.next();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: число " + positiveInteger + " должно быть положительным. Введите число заново");
+                positiveInteger = scan.next();
+            }
+        }
+        return number;
+    }
+
     private static void showResult(double result) {
         System.out.print("Результат вычислений: ");
         if (result % 1 == 0) {
@@ -36,7 +57,8 @@ class CalculatorTest {
         if (choice.equals("no")) {
             System.out.println("Спасибо, что воспользовались калькулятором");
             return true;
-        } else if (choice.equals("yes")) {
+        }
+        if (choice.equals("yes")) {
             System.out.print("Давайте попробуем еще раз :)");
         } else {
             isNext();
